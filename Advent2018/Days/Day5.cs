43d452f3249110e81724e9f021717be3deb8b5d0
Elian Kurtenkov <elian.kurtenkov@programista.pro>
2017-12-13 17:47:43 +0200

    Up to day 5

--- Advent2018/Days/Day5.cs
+++ Advent2018/Days/Day5.cs
@@ -11,7 +11,7 @@ namespace Advent2018
     {
         public void RunTests()
         {
-            var tests = new string[] { "abcde fghij", "abcde xyz ecdab", "a ab abc abd abf abj", "iiii oiii ooii oooi oooo", "oiii ioii iioi iiio" };
+            var tests = new string[] { "0\r\n3\r\n0\r\n1\r\n-3" };
             foreach (var i in tests)
             {
                 Console.WriteLine($"{i}: {GetResult2(i)}");
@@ -20,12 +20,55 @@ namespace Advent2018
 
         public string GetResult1(string input)
         {
-           
+            var instructions = ParseInpit(input);
+
+            var stepsCount = 0;
+            var pos = 0;
+
+            while (pos >= 0 && pos < instructions.Count)
+            {
+                var newPos = pos + instructions[pos];
+                instructions[pos]++;
+                pos = newPos;
+                stepsCount++;
+            }
+
+            return stepsCount.ToString(); //360603 - correct
         }
 
         public string GetResult2(string input)
         {
-       
+            var instructions = ParseInpit(input);
+
+            var stepsCount = 0;
+            var pos = 0;
+
+            while (pos >= 0 && pos < instructions.Count)
+            {
+                var newPos = pos + instructions[pos];
+
+                if (instructions[pos] >= 3)
+                    instructions[pos]--;
+                else
+                    instructions[pos]++;
+
+                pos = newPos;
+                stepsCount++;
+            }
+
+            return stepsCount.ToString(); //25347697 - correct
+        }
+
+        private List<int> ParseInpit(string input)
+        {
+            var lines = input.Split(Environment.NewLine);
+            var l = new List<int>();
+            foreach (var line in lines)
+            {
+                l.Add(int.Parse(line));
+            }
+
+            return l;
         }
     }
 }
---
+++ Advent2018/Days/Day6.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Advent2018
+{
+    public class Day6 : IDay
+    {
+        public void RunTests()
+        {
+            var tests = new string[] { "0\r\n3\r\n0\r\n1\r\n-3" };
+            foreach (var i in tests)
+            {
+                Console.WriteLine($"{i}: {GetResult2(i)}");
+            }
+        }
+
+        public string GetResult1(string input)
+        {
+               
+        }
+
+        public string GetResult2(string input)
+        {
+            return "";
+        }
+
+        private List<int> ParseInpit(string input)
+        {
+            var numbers = input.Split("\t");
+            var l = new List<int>();
+            foreach (var number in numbers)
+            {
+                l.Add(int.Parse(number));
+            }
+
+            return l;
+        }
+    }
+}
